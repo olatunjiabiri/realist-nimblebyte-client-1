1b1d076b8b0dbd0601949b55904d07af705a809b
Olanrewaju Omotayo <olanrewaju.omotayo@korpeatech.com>
2023-05-26 17:34:08 +0100

    AB#34 page logo and menu redesign

--- src/components/nav/Main.js
+++ src/components/nav/Main.js
@@ -28,82 +28,108 @@ export default function Main() {
   };
 
   return (
-    <nav className="nav d-flex justify-content-between lead">
-      <NavLink className="nav-link" aria-current="page" to="/">
-        Realist Korpea
-      </NavLink>
+    <>
+      <div className="row">
+        <div className="col">
+          <nav className="nav d-flex  lead">
+            <NavLink className="nav-link" aria-current="page" to="/search">
+              Search
+            </NavLink>
 
-      <NavLink className="nav-link" aria-current="page" to="/search">
-        Search
-      </NavLink>
+            <NavLink className="nav-link" aria-current="page" to="/buy">
+              Buy
+            </NavLink>
 
-      <NavLink className="nav-link" aria-current="page" to="/buy">
-        Buy
-      </NavLink>
+            <NavLink className="nav-link" aria-current="page" to="/rent">
+              Rent
+            </NavLink>
 
-      <NavLink className="nav-link" aria-current="page" to="/rent">
-        Rent
-      </NavLink>
-
-      <NavLink className="nav-link" aria-current="page" to="/agents">
-        Our Agents
-      </NavLink>
-
-      {loggedIn ? (
-        <a className="nav-link pointer" onClick={handlePostAdClick}>
-          Post Ad
-        </a>
-      ) : (
-        ""
-      )}
-
-      {!loggedIn ? (
-        <>
-          <NavLink className="nav-link" to="/login">
-            Login/Register
-          </NavLink>
-        </>
-      ) : (
-        ""
-      )}
-
-      {loggedIn ? (
-        <div className="dropdown">
-          <li>
-            <a
-              className="nav-link dropdown-toggle pointer"
-              data-bs-toggle="dropdown"
-            >
-              {auth?.name ? auth?.user?.name : auth?.user?.email?.split("@")[0]}
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <NavLink className="nav-link" to="/dashboard">
-                  Dashboard
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to="/user/profile">
-                  Update profile
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to="/user/settings">
-                  Change Password
-                </NavLink>
-              </li>
-              <hr />
-              <li>
-                <a onClick={logout} className="nav-link">
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </li>
+            <NavLink className="nav-link" aria-current="page" to="/agents">
+              Our Agents
+            </NavLink>
+          </nav>
         </div>
-      ) : (
-        ""
-      )}
-    </nav>
+
+        <div className="col">
+          <nav
+            className="nav d-flex justify-content-center align-items-center lead"
+            style={{ marginTop: "-3%" }}
+          >
+            <img
+              src="./logo.png"
+              alt="Korpea"
+              to={"/"}
+              height={70}
+              width={100}
+            />
+            <NavLink className="nav-link" aria-current="page" to="/">
+              Realist Korpea
+            </NavLink>
+          </nav>
+        </div>
+
+        <div className="col">
+          <nav className="nav d-Fflex  lead float-right">
+            {loggedIn ? (
+              <a className="nav-link pointer" onClick={handlePostAdClick}>
+                Post Ad
+              </a>
+            ) : (
+              ""
+            )}
+
+            {!loggedIn ? (
+              <>
+                <NavLink className="nav-link" to="/login">
+                  Login/Register
+                </NavLink>
+              </>
+            ) : (
+              ""
+            )}
+
+            {loggedIn ? (
+              <div className="dropdown">
+                <li>
+                  <a
+                    className="nav-link dropdown-toggle pointer"
+                    data-bs-toggle="dropdown"
+                  >
+                    {auth?.name
+                      ? auth?.user?.firstname
+                      : auth?.user?.email?.split("@")[0]}
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <NavLink className="nav-link" to="/dashboard">
+                        Dashboard
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="nav-link" to="/user/profile">
+                        Update profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink className="nav-link" to="/user/settings">
+                        Change Password
+                      </NavLink>
+                    </li>
+                    <hr />
+                    <li>
+                      <a onClick={logout} className="nav-link">
+                        Logout
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </div>
+            ) : (
+              ""
+            )}
+          </nav>
+        </div>
+      </div>
+    </>
   );
 }
