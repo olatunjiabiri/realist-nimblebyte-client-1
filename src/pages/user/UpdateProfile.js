b82c7f0e17f2cad24dcdf093862889739438b7b3
Olanrewaju Omotayo <olanrewaju.omotayo@korpeatech.com>
2023-06-09 17:57:01 +0100

    notifications updated AB#30

--- src/pages/user/UpdateProfile.js
+++ src/pages/user/UpdateProfile.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import axios from "axios";
-// import toast from "react-hot-toast";
 import { toast } from "react-toastify";
 
 import { useNavigate } from "react-router-dom";
