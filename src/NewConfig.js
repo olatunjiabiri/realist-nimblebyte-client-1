54c535b70c93fed73596e53070b20a0831bfa53d
Olanrewaju Omotayo <omotayo.olanrewaju@nimble-byte.com>
2023-09-15 16:24:37 +0100

    cleanup

--- src/NewConfig.js
+++ src/NewConfig.js
@@ -1,15 +1,4 @@
-// export const API = "https://realist-korpea-backend.azurewebsites.net/api";
-
-// export const API = "http://localhost:8000/api";
-// export const GOOGLE_PLACES_KEY = "AIzaSyDeaCvuV2krPlpVWEzGcQ2cWwmc5s5k4NU";
-// export const GOOGLE_MAPS_KEY = "AIzaSyCed96G-hxME8bDpJZB26MkROOKenNUwuc";
-
-// export const STRIPE_PUBLISHABLE_KEY="pk_test_51N5rmAIPzi27NxkuwY7kRWxrd45VLdOlaFpegNGUksbkeGd4CTnOlDcdHwU7u1yv1Cgln0s36kjMtN9XuiLtXbHK00cbwxDnxX"
-
-// dfudfufu
-
 const config = {
-  // API: "https://realist-korpea-backend.azurewebsites.net/api",
   API: "https://realist-node-backend.azurewebsites.net/api",
   // API: "http://localhost:8000/api",
   GOOGLE_PLACES_KEY: "AIzaSyDeaCvuV2krPlpVWEzGcQ2cWwmc5s5k4NU",
