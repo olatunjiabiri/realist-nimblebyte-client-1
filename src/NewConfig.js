e4dd095e505c676a6ad815e91a767d4eebc09b1f
Olanrewaju Omotayo <omotayo.olanrewaju@nimble-byte.com>
2023-09-16 08:01:59 +0100

    google api key updated

--- src/NewConfig.js
+++ src/NewConfig.js
@@ -1,7 +1,7 @@
 const config = {
   API: "https://realist-node-backend.azurewebsites.net/api",
   // API: "http://localhost:8000/api",
-  GOOGLE_PLACES_KEY: "AIzaSyDeaCvuV2krPlpVWEzGcQ2cWwmc5s5k4NU",
+  GOOGLE_PLACES_KEY: "AIzaSyD3IfqOASixLFAOqv7dDtwllrpHsa11iTs",
   GOOGLE_MAPS_KEY: "AIzaSyD3IfqOASixLFAOqv7dDtwllrpHsa11iTs",
   STRIPE_PUBLISHABLE_KEY:
     "pk_test_51N5rmAIPzi27NxkuwY7kRWxrd45VLdOlaFpegNGUksbkeGd4CTnOlDcdHwU7u1yv1Cgln0s36kjMtN9XuiLtXbHK00cbwxDnxX",
