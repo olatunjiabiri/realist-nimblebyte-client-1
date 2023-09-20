6c58d21937fac6dc750d4949b031a88e319c4007
Olatunji Abiri <olatunji.abiri@korpeatech.com>
2023-09-20 09:17:12 -0500

    Changes made in the UI

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -5,45 +5,32 @@ export default function Footer() {
     <div className="container-fluid">
       <div className="row align-items-left text-left p-4 bg-dark text-light mt-4">
         <div className="col-8">
-          <h4 className="mt-4">Realist App - Buy Sell or Rent Properties</h4>
+          <h4 className="mt-4">Realist App - Buy, Sell or Rent Properties</h4>
           <p className="mt-3">
             &copy; {new Date().getFullYear()} All rights reserved
           </p>
         </div>
 
         <div className="col-4 text-light">
-          <ul className="link footer-color" style={{ listStyleType: "none" }}>
-            <li>Quick Links</li>
-            <li>
-              {/* <Link style={{ color: "white" }} to="/"> */}
-              <Link className="link footer-color" to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="link footer-color" to="/search">
-                Search
-              </Link>
-            </li>
-            <li>
-              <Link className="link footer-color" to="/buy">
-                Buy
-              </Link>
-            </li>
-            <li>
-              <Link className="link footer-color" to="/rent">
-                Rent
-              </Link>
-            </li>
-            <li>
-              <Link className="link footer-color" to="/agents">
-                Our Agents
-              </Link>
-            </li>
-          </ul>
-          {/* <span align-item-end>
-            <Link> <i clasName="bi bi-chevron-up"></i>Top </Link>
-            </span> */}
+          <div className="link footer-color" style={{ listStyleType: "none" }}>
+            <div className="mt-4">Quick Links</div>
+          
+            <a href="/">
+              <img src="./home3.png" className="footer-nav-img" alt="Home" height={35} width={35} />
+            </a>
+            <a href="/search">
+            <img src="./search.png" className=" footer-nav-img" alt="search" height={40} width={40} />
+            </a>
+            <a href="/buy">
+            <img src="./buy.png" className=" footer-nav-img" alt="buy" height={35} width={35} />
+            </a>
+            <a href="/rent">
+            <img src="./rent.png" className=" footer-nav-img" alt="rent" height={35} width={35} />
+            </a>
+            <a href="/agents">
+            <img src="./ouragents.png" className=" footer-nav-img" alt="ouragents" height={35} width={35} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
