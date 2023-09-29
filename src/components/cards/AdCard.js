15b740a2a8f362b81c7698ecdb59d0dcee58bfde
Olanrewaju Omotayo <omotayo.olanrewaju@nimble-byte.com>
2023-09-29 16:55:08 +0100

    AdCard updated

--- src/components/cards/AdCard.js
+++ src/components/cards/AdCard.js
@@ -19,13 +19,13 @@ export default function AdCard({ ad }) {
 
   return (
     <div className="d-flex col-lg-4 p-4 gx-4 gy-4 col-md-6 col-sm-6">
-      {/* <Link className="link" to={`/ad/${ad.slug}`}> */}
-      <Badge.Ribbon
-        text={`${ad?.type} for ${ad?.action === "Sell" ? "Sale" : "Rent"}`}
-        color={`${ad?.action === "Sell" ? "blue" : "blue"}`}
-      >
-        <div className="card hoverable p-2 shadow-lg">
-          <Link className="link" to={`/ad/${ad.slug}`}>
+      <Link className="link" to={`/ad/${ad.slug}`}>
+        <Badge.Ribbon
+          text={`${ad?.type} for ${ad?.action === "Sell" ? "Sale" : "Rent"}`}
+          color={`${ad?.action === "Sell" ? "blue" : "blue"}`}
+        >
+          <div className="card hoverable p-2 shadow-lg">
+            {/* <Link className="link" to={`/ad/${ad.slug}`}> */}
             <img
               src={ad?.photos?.[0].Location}
               alt={`${ad?.type}-${ad?.address}-${ad?.action}-${ad?.price}`}
@@ -36,43 +36,43 @@ export default function AdCard({ ad }) {
                 objectFit: "cover",
               }}
             />
-          </Link>
+            {/* </Link> */}
 
-          <div className="card-body ad-card-body">
-            <div className="d-flex justify-content-between">
-              <h3 className="pt-1">
-                {" "}
-                <span>&#8358;</span>
-                {/* {formatNumber(ad?.price)} */}
-                {millify(ad?.price)}
-              </h3>
-              <section className="like-unlike-button">
-                <LikeUnlike ad={ad} />
-              </section>
-            </div>
+            <div className="card-body ad-card-body">
+              <div className="d-flex justify-content-between">
+                <h3 className="pt-1">
+                  {" "}
+                  <span>&#8358;</span>
+                  {/* {formatNumber(ad?.price)} */}
+                  {millify(ad?.price)}
+                </h3>
+                <section className="like-unlike-button">
+                  <LikeUnlike ad={ad} />
+                </section>
+              </div>
 
-            <p className="card-text">{ad?.address}</p>
+              <p className="card-text">{ad?.address}</p>
 
-            <AdFeatures ad={ad} />
-            <div className="d-flex justify-content-between">
-              <p className="pt-3 card-text">
-                Posted {dayjs(ad?.createdAt).fromNow()}
-              </p>
-              <p className="justify-content-end">
-                <HashLink
-                  className="bg-white"
-                  smooth
-                  to={`/ad/${ad.slug}/#contact-owner`}
-                  scroll={(el) => scrollWithOffset(el)}
-                >
-                  <ContactOwnerButton />
-                </HashLink>
-              </p>
+              <AdFeatures ad={ad} />
+              <div className="d-flex justify-content-between">
+                <p className="pt-3 card-text">
+                  Posted {dayjs(ad?.createdAt).fromNow()}
+                </p>
+                <p className="justify-content-end">
+                  <HashLink
+                    className="bg-white"
+                    smooth
+                    to={`/ad/${ad.slug}/#contact-owner`}
+                    scroll={(el) => scrollWithOffset(el)}
+                  >
+                    <ContactOwnerButton />
+                  </HashLink>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </Badge.Ribbon>
-      {/* </Link> */}
+        </Badge.Ribbon>
+      </Link>
     </div>
   );
 }
