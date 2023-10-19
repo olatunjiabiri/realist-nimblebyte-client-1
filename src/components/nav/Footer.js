0fb92c7e35e1678aa9483a9cb60d69eb970a15ca
bolaleye <olabiyi.olaleye@korpeatech.com>
2023-10-19 08:48:20 -0500

    footer

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -6,16 +6,17 @@ export default function Footer() {
   return (
     <div className="container-fluid">
       <div className="row align-items-left text-left p-4 bg-dark text-light pt-4">
-        <div className="col-8">
+        <div className="col-12 col-md-8">
           <h4 className="mt-4">Realist App - Buy, Sell or Rent Properties</h4>
           <p className="mt-3">
             &copy; {new Date().getFullYear()} All rights reserved
           </p>
         </div>
 
-        <div className="col-4 text-light">
+        <div className="col-12 col-md-4 text-light">
+        <div className="mt-4 fs-3">Quick Links</div>
           <div className="link footer-color" style={{ listStyleType: "none" }}>
-            <div className="mt-4">Quick Links</div>
+            
 
             <Link to="/">
               <img
