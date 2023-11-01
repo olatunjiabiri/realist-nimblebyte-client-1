cfc6d2c8f39b4f23258ac52c09722206603ad3e5
Olanrewaju Omotayo <omotayo.olanrewaju@nimble-byte.com>
2023-11-01 23:06:17 +0100

    contact us created

--- src/pages/contactUs/ContactUs.js
+++ src/pages/contactUs/ContactUs.js
@@ -20,6 +20,7 @@ const ContactUs = () => {
 
   const onSubmit = async (values, actions) => {
     const { contactName, email, message, phone, subject } = values;
+    console.log("subject", subject);
 
     try {
       // console.log(email, password);
@@ -71,138 +72,146 @@ const ContactUs = () => {
   });
 
   return (
-    <ContentWrapper>
-      <div className="row">
-        <div className="col-md-4 offset-md-4">
-          <div className="bd-1 form-container bd-color">
-            <div className="contact-form-title text-center">
-              <span>Contact Us</span>
-            </div>
-            <form onSubmit={handleSubmit}>
-              <div className="form-group mt-4">
-                {/* <label for="name" className="control-label form-label1">
+    // <ContentWrapper>
+    <div className="background-color">
+      <div className="container mt-5 p-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <div className="bd-1 contact-us-container contact-bd-color">
+              <div className="contact-form-title text-center">
+                <h2>Contact Us</h2>
+              </div>
+              <hr />
+
+              <form onSubmit={handleSubmit}>
+                <div className="form-group mt-4">
+                  {/* <label for="name" className="control-label form-label1">
               {" "}
               Name{" "}
             </label> */}
 
-                <input
-                  type="text"
-                  className="form-control mb-3"
-                  placeholder="Enter your name"
-                  name="contactName"
-                  id="name"
-                  value={values.contactName}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                {errors.contactName && touched.contactName && (
-                  <p className="mt-0 text-danger">
-                    <small>
-                      {" "}
-                      <div> {errors.contactName}</div>
-                    </small>
-                  </p>
-                )}
-              </div>
-              <div className="form-group mt-4">
-                {/* <label className="control-label form-label1"> Email </label> */}
-                <input
-                  type="email"
-                  className="form-control mb-3"
-                  name="email"
-                  placeholder="Enter your email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                {errors.email && touched.email && (
-                  <p className="mt-0 text-danger">
-                    <small>
-                      {" "}
-                      <div> {errors.email}</div>
-                    </small>
-                  </p>
-                )}
-              </div>
-              <div className="form-group mt-4">
-                {/* <label className="control-label form-label1"> Phone </label> */}
-                <input
-                  type="tel"
-                  name="phone"
-                  className="form-control mb-3"
-                  placeholder="Enter your email"
-                  value={values.phone}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                {errors.phone && touched.phone && (
-                  <p className="mt-0 text-danger">
-                    <small>
-                      {" "}
-                      <div> {errors.phone}</div>
-                    </small>
-                  </p>
-                )}
-              </div>
+                  <input
+                    type="text"
+                    className="form-control mb-3"
+                    placeholder="Enter your name"
+                    name="contactName"
+                    id="name"
+                    value={values.contactName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.contactName && touched.contactName && (
+                    <p className="mt-0 text-danger">
+                      <small>
+                        {" "}
+                        <div> {errors.contactName}</div>
+                      </small>
+                    </p>
+                  )}
+                </div>
+                <div className="form-group mt-4">
+                  {/* <label className="control-label form-label1"> Email </label> */}
+                  <input
+                    type="email"
+                    className="form-control mb-3"
+                    name="email"
+                    placeholder="Enter your email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <p className="mt-0 text-danger">
+                      <small>
+                        {" "}
+                        <div> {errors.email}</div>
+                      </small>
+                    </p>
+                  )}
+                </div>
+                <div className="form-group mt-4">
+                  {/* <label className="control-label form-label1"> Phone </label> */}
+                  <input
+                    type="tel"
+                    name="phone"
+                    className="form-control mb-3"
+                    placeholder="Enter your email"
+                    value={values.phone}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.phone && touched.phone && (
+                    <p className="mt-0 text-danger">
+                      <small>
+                        {" "}
+                        <div> {errors.phone}</div>
+                      </small>
+                    </p>
+                  )}
+                </div>
 
-              <div className="form-group mt-4">
-                <select
-                  // autoFocus
-                  placeholder="Select Message type"
-                  onChange={handleChange}
-                  className="form-select form-select-lg"
-                >
-                  <option disabled selected>
+                <div className="form-group mt-4">
+                  <select
+                    // autoFocus
+                    placeholder="Select Message type"
+                    onChange={handleChange}
+                    name="subject"
+                    className="form-select form-select-lg"
+                  >
+                    {/* <option disabled selected>
                     {" "}
                     Select Message type
-                  </option>
-                  <option className="form-select-lg mb-5" value={"Enquiry"}>
-                    Enquiry
-                  </option>
-                  <option className="form-select-lg mb-5" value={"Complaint"}>
-                    Complaint
-                  </option>
-                </select>
-              </div>
+                  </option> */}
+                    <option className="form-select-lg mb-3" value={"Enquiry"}>
+                      Enquiry
+                    </option>
+                    <option className="form-select-lg mb-3" value={"Complaint"}>
+                      Complaint
+                    </option>
+                  </select>
+                </div>
 
-              <div className="form-group mt-4">
-                {/* <label className="form-label">
+                <div className="form-group mt-4">
+                  {/* <label className="form-label">
               {" "}
               Message <span className="text-muted">(optional)</span>
             </label> */}
-                <textarea
-                  className="form-control mb-3"
-                  placeholder="Write your message"
-                  cols={10}
-                  rows={5}
-                  name="message"
-                  value={values.message}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  // onChange={(e) => setMessage(e.target.value)}
-                />
-                {errors.message && touched.message && (
-                  <p className="mt-0 text-danger">
-                    <small>
-                      {" "}
-                      <div> {errors.message}</div>
-                    </small>
-                  </p>
-                )}
-              </div>
-              <div className="mt-4">
-                <button
-                  className="contact-button w-100 btn btn-color"
-                  disabled={loading}
-                >
-                  {isSubmitting ? "Loading" : "Submit"}
-                </button>
-              </div>
-            </form>
+                  <textarea
+                    className="form-control mb-3"
+                    placeholder="Write your message"
+                    cols={10}
+                    rows={5}
+                    name="message"
+                    value={values.message}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    // onChange={(e) => setMessage(e.target.value)}
+                  />
+                  {errors.message && touched.message && (
+                    <p className="mt-0 text-danger">
+                      <small>
+                        {" "}
+                        <div> {errors.message}</div>
+                      </small>
+                    </p>
+                  )}
+                </div>
+                <div className="mt-4">
+                  <button
+                    className="contact-button w-100 btn btn-color"
+                    disabled={loading}
+                  >
+                    {isSubmitting ? "Loading" : "Submit"}
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </ContentWrapper>
+    </div>
+
+    // </ContentWrapper>
   );
 };
 
