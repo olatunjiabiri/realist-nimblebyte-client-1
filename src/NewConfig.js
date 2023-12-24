7590bbfc34c14b9bf172942293cbee2092f7304c
Olanrewaju Omotayo <omotayo.olanrewaju@nimble-byte.com>
2023-12-24 15:24:49 +0100

    admin email updated

--- src/NewConfig.js
+++ src/NewConfig.js
@@ -1,13 +1,13 @@
 const config = {
-  AdminEmail: "information@nimble-byte.com",
+  AdminEmail: "support@nimblecasa.com",
 
   APIS_PROTECT: "Fm2dT0GzyUa_Q_6KxMTzNwELPcEVcHOUqyjd0yn0LJqA",
   AUTH_API_KEY: "d6cf4f00-773e-40b6-aed1-1960691e954e",
   CLIENT_BASE_URL: "https://realistclientapp2.azurewebsites.net",
   API:
     // process.env.NODE_ENV === "production"
-    // "http://localhost:8000/api",
-    "https://realist-node-backend.azurewebsites.net/api",
+    "http://localhost:8000/api",
+  // "https://realist-node-backend.azurewebsites.net/api",
   AUTH_API:
     // process.env.NODE_ENV === "production"
     //"https://localhost:7298",
