ee2762eef11d4268930f6936f23062adae0de20f
Olanrewaju Omotayo <omotayo.olanrewaju@nimble-byte.com>
2024-01-26 19:16:46 +0100

    undo committed config file

--- src/components/cookies/CookiesPolicy.js
+++ src/components/cookies/CookiesPolicy.js
@@ -15,17 +15,19 @@ function CookiePolicy() {
   };
 
   return (
-    <div>
+    <div className="d-flex justify-content-center">
       {showCookieConsent && (
         <CookieConsent
           debug={true}
           location="bottom"
           buttonText={<AiOutlineCloseCircle />}
-          // buttonText=""
           cookieName="myCookie"
           style={{
             background: "#003366",
             alignContent: "center",
+            width: "50%",
+            position: "fixed",
+            // left: "50%",
           }}
           buttonStyle={{
             background: "none",
