972c25c0a058cd605ff4fd6d18a8ec1ddc5f8ae1
omotayo olanrewaju <omotayo.olanrewaju@nimble-byte.com>
2024-02-20 20:52:37 +0100

    changes updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,63 +1,18 @@
-import React, { useEffect, useState, useRef } from "react";
-import axios from "axios";
-import { ShimmerPostList } from "react-shimmer-effects";
-import { setKey, geocode, RequestType } from "react-geocode";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
+import React, { useEffect, useState } from "react";
 
-import { useAuth } from "../context/auth";
-import AdCard from "../components/cards/AdCard";
 import SearchForm from "../components/forms/SearchForm";
 import LogoutMessage from "../components/misc/logoutMessage/LogoutMessage";
 
-import RowPerPage from "../components/rowPerPage/RowPerPage.js";
 import FeaturedProperties from "../components/featuredProperties/FeaturedProperties.js";
+import AdsForSale from "../components/adsForSale/AdsForSale.js";
 
 export default function Home() {
-  let count = 0;
-  count++;
-
-  // context
-  const [auth] = useAuth();
-
-  // state
-  const [ads, setAds] = useState([]);
-  const [total, setTotal] = useState(0);
-  const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(9);
   const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (auth.user === null) {
-      auth.token = "";
-    }
-    fetchAds();
-  }, []);
-
-  useEffect(() => {
-    fetchAds();
-  }, [page, perPage]);
-
-  const fetchAds = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(`/ads/${page}/${perPage}`);
-      setAds(data.ads.filter((a) => !a.featuredPropertyStatus));
-
-      setTotal(data.total);
-      setLoading(false);
-    } catch (err) {
-      console.log(err);
-      setLoading(false);
-    }
-  };
-  const handleChange = (event, value) => {
-    setPage(value);
-  };
+  const [featuredPropertyLoading, setFeaturedPropertyLoading] = useState(true);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [ads]);
+  }, [loading]);
 
   return (
     <div>
@@ -67,59 +22,15 @@ export default function Home() {
         </div>
 
         <div className="container mt-3 px-4">
-          <FeaturedProperties />
+          <FeaturedProperties
+            featuredPropertyLoading={featuredPropertyLoading}
+            setFeaturedPropertyLoading={setFeaturedPropertyLoading}
+          />
         </div>
 
         <div className="container pt-3 px-4">
-          <div className="row d-flex justify-content-evenly">
-            <h3 className="mt-5 text-center">Properties For Sale and Rent</h3>
-
-            {loading ? (
-              <div style={{ padding: "40px 0" }}>
-                <ShimmerPostList
-                  postStyle="STYLE_FOUR"
-                  col={3}
-                  row={2}
-                  gap={30}
-                />
-              </div>
-            ) : (
-              ads?.map((ad) => <AdCard ad={ad} key={ad._id} />)
-            )}
-          </div>
-
-          {ads?.length < total ? (
-            <div className="row">
-              <div className="col text-center mt-4 mb-4">
-                <Stack spacing={2}>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Pagination
-                      color="primary"
-                      shape="rounded"
-                      showFirstButton
-                      showLastButton
-                      variant="outlined"
-                      count={Math.ceil(total / perPage)}
-                      page={page}
-                      onChange={handleChange}
-                    />
-                    <RowPerPage
-                      total={total}
-                      rowPerPage={perPage}
-                      setRowPerPage={setPerPage}
-                    />
-                  </div>
-                </Stack>
-              </div>
-            </div>
-          ) : (
-            ""
+          {!featuredPropertyLoading && (
+            <AdsForSale setLoading={setLoading} loading={loading} />
           )}
         </div>
       </LogoutMessage>
