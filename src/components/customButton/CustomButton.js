9038a9bcf7ea9061ebf5f1f57ce27ccfc28d4a93
omotayo olanrewaju <omotayo.olanrewaju@nimble-byte.com>
2024-03-05 13:16:23 +0100

    sample usage added

--- src/components/customButton/CustomButton.js
+++ src/components/customButton/CustomButton.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./CustomButton.css";
 
-// CustomButton component with label and style props
+// sample of how to use this reusable component
+
+/* <div className="">
+          <CustomButton label="Customed Button" />
+          <span>{"  "}</span>
+
+          <CustomButton
+            className="outlined-button"
+            label="Outlined Customed Button"
+          />
+        </div> */
+
 const CustomButton = ({ label, className, style }) => {
   return (
     <button className={`custom-button ${className}`} style={style}>
