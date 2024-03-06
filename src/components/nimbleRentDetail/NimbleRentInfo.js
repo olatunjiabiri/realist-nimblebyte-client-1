dd1ca537b36a6809c9a411c2f29a69e035735111
Olatunji Abiri <olatunji.abiri@korpeatech.com>
2024-03-06 14:14:28 +0100

    changes made to the css file and the js

--- src/components/nimbleRentDetail/NimbleRentInfo.js
+++ src/components/nimbleRentDetail/NimbleRentInfo.js
@@ -1,141 +1,3 @@
-// import React, { useEffect } from "react";
-// import Step from "@mui/material/Step";
-// import Stepper from "@mui/material/Stepper";
-// import StepButton from "@mui/material/StepButton";
-// import StepLabel from "@mui/material/StepLabel";
-// import StepContent from "@mui/material/StepContent";
-// import Typography from "@mui/material/Typography";
-
-// import "./RentDetailsPage.css";
-
-// const RentDetailsPage = () => {
-//   useEffect(() => {
-//     // Scroll to the top of the page when the component mounts
-//     window.scrollTo(0, 0);
-//   }, []);
-
-//   const steps = [
-//     {
-//       label: "Search properties on Nimble rent",
-//       content: (
-//          <p>
-//           Select a desired apartment from properties available on Nimble rent.
-//          </p>
-//        ),
-//     },
-//     {
-//       label: "Payment option",
-//       content: (
-//         <p>
-//           Ensure you check use Nimble rent to pay while submitting property
-//           request.
-//         </p>
-//       ),
-//     },
-//     {
-//       label: "Verification",
-//       content: (
-//         <p>
-//           We will reach out to you to help confirm the necessary information to
-//           approve your loan.
-//         </p>
-//       ),
-//     },
-//     {
-//       label: "Access to loan",
-//       content: (
-//         <p>
-//           Once verification is complete and you meet all eligibility criteria,
-//           your loan is disbursed.
-//         </p>
-//       ),
-//     },
-//   ];
-
-//   return (
-//     <div className="rent-details-container">
-//       <h2 className="main-title">
-//         You can rent now and pay later @Nimble Rent
-//       </h2>
-//       <p className="title-subtexts">
-//         NimbleRent is a subsidiary of NimbleCasa that offers a RNPL(Rent Now Pay
-//         Later) service. We allow our valued /n customers to secure their desired
-//         property for rent immediately and pay back the rental amount
-//         inconvenient installments. We have collaborated with some financial
-//         institutions that are willing to offer loan services.
-//       </p>
-
-//       <div className="cards-container">
-//         <div className="card-one-container">
-//           <div>
-//             <div className="card-one-title-container">
-//               <h3 className="card-one-title">Eligibility Status</h3>
-//               <div className="card-one-subtitle-box">
-//                 <p className="subtitle-box-text">
-//                   Please note that only Civil Servants are qualified for the
-//                   offer.
-//                 </p>
-//               </div>
-//             </div>
-
-//             <div className="card-one-inner-text-container">
-//               <p className="subtitle-text">
-//                 The followings are the eligible criteria when using Nimble rent.
-//               </p>
-//               <ul className="listed-style">
-//                 <li>
-//                   Your salary account must be operational for at least 6 months.
-//                 </li>
-//                 <li>Your account number must be linked to BVN.</li>
-//                 <li>Correctly updated phone numbers.</li>
-//                 <li>Must have positive credit record with Credit Bureaus.</li>
-//                 <li>
-//                   Minimum Loan Amount: N500,000 (Five Hundred Thousand Naira
-//                   only).
-//                 </li>
-//                 <li>Convenient repayment period up to 12 months.</li>
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="card-two-container">
-//           <div className="card-two-inner-container">
-//             <div className="stepper-title-container">
-//               <h3 className="stepper-title">How it works</h3>
-//               <p className="stepper-subtext">
-//                 Quick and easy steps to use Nimble rent!
-//               </p>
-//             </div>
-
-//             <div className="stepper-container">
-//               <Stepper
-//                 className="label-content"
-//                 activeStep={0}
-//                 orientation="vertical"
-//               >
-//                 {steps.map((step, index) => (
-//                   <Step key={index}>
-//                     <StepButton>
-//                       <StepLabel>
-//                         {step.label}
-//                       </StepLabel>
-//                       <StepContent>
-//                       {step.content}
-//                       </StepContent>
-//                     </StepButton>
-//                   </Step>
-//                 ))}
-//               </Stepper>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RentDetailsPage;
-
 import React, { useEffect } from "react";
 import Step from "@mui/material/Step";
 import Stepper from "@mui/material/Stepper";
@@ -145,12 +7,7 @@ import StepContent from "@mui/material/StepContent";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-import "./RentDetailsPage.css";
-
-// Define the CSS class for bold labels
-const labelStyles = {
-  fontWeight: "bold",
-};
+import "./NimbleRentInfo.css";
 
 // Define the OrangeStepIcon function
 function OrangeStepIcon(index) {
@@ -175,14 +32,7 @@ function OrangeStepIcon(index) {
     );
   };
 }
-
-const RentDetailsPage = () => {
-  useEffect(() => {
-    // Scroll to the top of the page when the component mounts
-    window.scrollTo(0, 0);
-  }, []);
-
-  // Define the steps data array
+const NimbleRentInfo = () => {
   const steps = [
     {
       label: "Search properties on Nimble rent",
@@ -207,93 +57,101 @@ const RentDetailsPage = () => {
   ];
 
   return (
-    <div className="rent-details-container">
-      <div className="main-title-container">
-        <h2 className="main-title">
-          You can rent now and pay later{" "}
-          <span className="nimble-rent">Nimble Rent</span>
-        </h2>
-        <p className="title-subtexts">
-          NimbleRent is a subsidiary of NimbleCasa that offers a RNPL(Rent Now
-          Pay Later) service. We allow our valued customers to secure their
-          desired property for rent immediately and pay back the rental amount
-          inconvenient installments. We have collaborated with some financial
-          institutions that are willing to offer loan services.
-        </p>
-      </div>
-      <div className="cards-container">
-        <div className="card-one-container">
-          <div>
-            <div className="card-one-title-container">
-              <h3 className="card-one-title">Eligibility Status</h3>
-              <div className="card-one-subtitle-box">
-                <p className="subtitle-box-text">
-                  Please note that only Civil Servants are qualified for the
-                  offer.
+    <div className="nimble-rent-info-container">
+      <div className="rent-details-container">
+        <div className="main-title-container">
+          <h2 className="main-title">
+            You can rent now and pay later{" "}
+            <span className="nimble-rent">Nimble Rent</span>
+          </h2>
+          <p className="title-subtexts">
+            NimbleRent is a subsidiary of NimbleCasa that offers a RNPL(Rent Now
+            Pay Later) service. We allow our valued customers to secure their
+            desired property for rent immediately and pay back the rental amount
+            inconvenient installments. We have collaborated with some financial
+            institutions that are willing to offer loan services.
+          </p>
+        </div>
+        <div className="cards-container">
+          <div className="card-one-container">
+            <div>
+              <div className="card-one-title-container">
+                <h3 className="card-one-title">Eligibility Status</h3>
+                <div className="card-one-subtitle-box">
+                  <p className="subtitle-box-text">
+                    Please note that only Civil Servants are qualified for the
+                    offer.
+                  </p>
+                </div>
+              </div>
+
+              <div className="card-one-inner-text-container">
+                <p className="subtitle-text">
+                  The followings are the eligible criteria when using Nimble
+                  rent.
                 </p>
+                <div className="listed-style-container">
+                  <ul className="">
+                    <li>
+                      Your salary account must be operational for at least 6
+                      months.
+                    </li>
+                    <li>Your account number must be linked to BVN.</li>
+                    <li>Correctly updated phone numbers.</li>
+                    <li>
+                      Must have positive credit record with Credit Bureaus.
+                    </li>
+                    <li>
+                      Minimum Loan Amount: N500,000 (Five Hundred Thousand Naira
+                      only).
+                    </li>
+                    <li>Convenient repayment period up to 12 months.</li>
+                  </ul>
+                </div>
               </div>
             </div>
-
-            <div className="card-one-inner-text-container">
-              <p className="subtitle-text">
-                The followings are the eligible criteria when using Nimble rent.
-              </p>
-              <ul className="listed-style">
-                <li>
-                  Your salary account must be operational for at least 6 months.
-                </li>
-                <li>Your account number must be linked to BVN.</li>
-                <li>Correctly updated phone numbers.</li>
-                <li>Must have positive credit record with Credit Bureaus.</li>
-                <li>
-                  Minimum Loan Amount: N500,000 (Five Hundred Thousand Naira
-                  only).
-                </li>
-                <li>Convenient repayment period up to 12 months.</li>
-              </ul>
-            </div>
           </div>
-        </div>
-        <div className="card-two-container">
-          <div className="card-two-inner-container">
-            <div className="stepper-title-container">
-              <h3 className="stepper-title">How it works</h3>
-              <p className="stepper-subtext">
-                Quick and easy steps to use Nimble rent!
-              </p>
-            </div>
+          <div className="card-two-container">
+            <div className="card-two-inner-container">
+              <div className="stepper-title-container">
+                <h3 className="stepper-title">How it works</h3>
+                <p className="stepper-subtext">
+                  Quick and easy steps to use Nimble rent!
+                </p>
+              </div>
 
-            <div className="stepper-container">
-              {/* <Box sx={{ maxWidth: 400 }}> */}
-              <Stepper
-                className="label-content"
-                activeStep={steps.length - 1}
-                orientation="vertical"
-                // connector={<div style={{ color: "#EE7B0D", height: "6px" }} />}
-              >
-                {steps.map((step, index) => (
-                  <Step key={step.label} expanded>
-                    <StepLabel
-                      StepIconComponent={OrangeStepIcon(index)}
-                      // style={{ fontWeight: "bold" }}
-                    >
-                      <span className="step-label"> {step.label} </span>
-                    </StepLabel>
-                    <StepContent>
-                      <Typography
-                        sx={{
-                          borderLeft: "2px solid orange",
-                          marginLeft: -2.6,
-                          paddingLeft: 2.75,
-                        }}
-                      >
-                        {step.description}
-                      </Typography>
-                    </StepContent>
-                  </Step>
-                ))}
-              </Stepper>
-              {/* </Box> */}
+              <div className="stepper-container">
+                {/* <Box sx={{ maxWidth: 400 }}> */}
+                <Stepper
+                  className="label-content"
+                  activeStep={steps.length - 1}
+                  orientation="vertical"
+                >
+                  {steps.map((step, index) => (
+                    <Step key={step.label} expanded>
+                      <StepLabel StepIconComponent={OrangeStepIcon(index)}>
+                        <span className="step-label"> {step.label} </span>
+                      </StepLabel>
+                      <StepContent>
+                        <Typography
+                          sx={{
+                            borderLeft: "2px dotted orange",
+                            marginLeft: -2.6,
+                            paddingLeft: 2.75,
+                            marginBottom: 1.5,
+                          }}
+                        >
+                          {step.description}
+                        </Typography>
+                      </StepContent>
+                      {index < steps.length - 1 && (
+                        <Box sx={{ borderLeft: "2px dotted orange", ml: -1 }} />
+                      )}
+                    </Step>
+                  ))}
+                </Stepper>
+                {/* </Box> */}
+              </div>
             </div>
           </div>
         </div>
@@ -301,5 +159,4 @@ const RentDetailsPage = () => {
     </div>
   );
 };
-
-export default RentDetailsPage;
+export default NimbleRentInfo;
