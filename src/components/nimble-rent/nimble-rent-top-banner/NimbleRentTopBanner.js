170900f5a8e56c1ba6785977cd40a39455be8e44
omotayo olanrewaju <omotayo.olanrewaju@nimble-byte.com>
2024-03-06 15:33:34 +0100

    changes updated

--- src/components/nimble-rent/nimble-rent-top-banner/NimbleRentTopBanner.js
+++ src/components/nimble-rent/nimble-rent-top-banner/NimbleRentTopBanner.js
@@ -1,35 +1,47 @@
 import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import useTheme from "@mui/system/useTheme";
 
-import "./NimbleRentTopBanner.css";
 import CustomButton from "./../../customButton/CustomButton";
-import { Padding } from "@mui/icons-material";
+import "./NimbleRentTopBanner.css";
 
 const NimbleRentTopBanner = ({ showButton = true }) => {
+  const theme = useTheme();
+
+  const isSmScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const containerStyle = {
+    height: isSmScreen && showButton ? "109px" : "80px",
+  };
+
+  const buttonStyle = {
+    padding: "3px 8px",
+    fontSize: "12px",
+    fontWeight: 400,
+    lineHeight: "16px",
+  };
+
   return (
-    <div className="nimble-rent-top-banner-container">
+    <div className="nimble-rent-top-banner-container" style={containerStyle}>
       <div className="nimble-rent-top-banner-text">
         <div className="nimble-rent-top-banner-textx">
           <span className="nimble-rent-top-banner-texta">Hurray</span>&nbsp;
-          <span className="nimble-rent-top-banner-textb">Nimble rent</span>
+          <span className="nimble-rent-top-banner-texta">Nimble rent</span>
           &nbsp;
-          <span className="">
-            <img src="./fire.png"></img>
-            is here!!
+          <span className="nimble-rent-top-banner-textb">
+            <img src="./fire.png"></img>&nbsp;is here!!
           </span>
           &nbsp;
+          <span className="nimble-rent-top-banner-textc">
+            You can now rent a property and pay later
+          </span>
+          {showButton && (
+            <span className="nimble-rent-top-banner-button">
+              <CustomButton label="Check Eligibility" style={buttonStyle} />
+            </span>
+          )}
         </div>
-        <span className="nimble-rent-top-banner-textc">
-          You can now rent a property and pay later
-        </span>
       </div>
-      {showButton && (
-        <div className="nimble-rent-top-banner-button">
-          <CustomButton
-            label="Check Eligibility"
-            style={{ padding: "4px 12px" }}
-          />
-        </div>
-      )}
     </div>
   );
 };
